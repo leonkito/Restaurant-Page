98c116d6f05a655da8125d2700b0020350572a6d
Leonardo Niekiforuk <leonardoniekiforuk@hotmail.com>
2022-09-21 11:51:55 -0300

    Add logic for tab switch

--- script.js
+++ script.js
@@ -16,11 +16,42 @@ const Dish = (title,imgUrl,ingredients)=>{
     return{title,imgUrl,ingredients,createCard}
 }
 
-const crepioca = Dish("crepioca","img/broccoli.png","eggs,milk")
-const broccoli = Dish("Rosted Broccoli","img/broccoli.png","Broccoli, butter and parmesian")
+const crepioca = Dish("CREPIOCA  $12","img/crepioca.png","Tapioca, Eggs, Milk and cheese")
+const broccoli = Dish("BRAISED BROCCOLI  $15","img/broccoli.png","Broccoli, Butter and Parmesian")
+const spaghetti = Dish("WHITE SPAGHETTI $22","img/spaghetti.png","Spaghetti, Heavy Milk Cream, Butter and Lots of Parmesian")
+const caipirinha = Dish("caipirinha $10","img/caipirinha.png","Brazilian Cachaça, Sugar and Lime")
+const kiwi = Dish("Kiwi Caipirinha $10","img/kiwicapirinha.png","Vodka, cachaça, sugar and kiwi")
+const mojito = Dish("mojito $10","img/mojito.png","ingredients for a mojito here")
+const martini = Dish("Martini $10","img/martini.png","Dry martini")
 
-let Menu = [broccoli,crepioca,crepioca,crepioca]
+let Drinks = [caipirinha,kiwi,mojito,martini]
+let Menu = [broccoli,crepioca,spaghetti]
 
-for(let item of Menu){
-    item.createCard(content)
+const food = document.getElementById('food')
+const drink = document.getElementById('drink')
+const home = document.getElementById('home')
+
+let divButtons = [food,drink,home]
+
+food.addEventListener('click', function (){clearContent(content);createContent(Menu);setClick(food,divButtons)});
+drink.addEventListener('click', function (){clearContent(content);createContent(Drinks);setClick(drink,divButtons)});
+home.addEventListener('click', function (){clearContent(content);setClick(home,divButtons)});
+function setClick(element,arr){
+    for(let item of arr){
+        item.classList.remove("listClicked")
+        item.classList.add("listUnClicked")
+    }
+    element.classList.add("listClicked")
+    element.classList.remove("listUnClicked")
 }
+
+function clearContent(element){
+    element.innerHTML=""
+}
+function createContent(arr){
+    for(let item of arr){
+        item.createCard(content)
+    }
+}
+
+// Need to change color and background color of div buttons for menu,drinks and food
